ec7302f2806fd1b3d18fe8e5d63f9fa4ed74a2c1
Chetu14321 <chetuchethan87@gmail.com>
2025-08-26 11:02:08 +0530

    few

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -22,7 +22,7 @@ export default function Sidebar() {
       
       
       <div className="p-3 bg-light rounded shadow-sm text-center">
-        <h5 className="fw-bold mb-3">Promotions</h5>
+        <h5 className="fw-bold mb-3">comming soon</h5>
         <div style={{ overflowX: "auto" }}>
           <AdSlot height={250} width={250} /> {/* Leaderboard Banner */}
         </div>
